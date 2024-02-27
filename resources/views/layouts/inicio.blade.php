a7519065b2d1488d0b18bf45d92cf55d29f2301c
Lucas Fernandes <lucass20fernandess@gmail.com>
2024-02-27 18:15:37 -0300

    Componentes da tela de inicio

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -11,26 +11,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="{{ asset('build/assets/styles/componentes.css')}}">
+
+        <script src="https://cdn.tailwindcss.com"></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    <body class="font-sans antialiased static bg-gray-100">
+        @yield('tela-inicial')
     </body>
 </html>
